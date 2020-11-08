e7ae15be3f9dd10fd37049cf03f54bad11f1024e
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2020-11-08 20:47:33 +0530

    useState use for object update

--- react-hooks/src/components/UseState.js
+++ react-hooks/src/components/UseState.js
@@ -1,11 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const UseState = () => {
     // useState:: lets you use local state within a function component.
     const [count, setCount] = useState(0);
+    const [info, setInfo] = useState({ age: 19, siblingsNum: 4 })
 
     const handelReset = () => {
         setCount(0);
+    }
+
+    const { age, siblingsNum } = info;
+
+    const handelInfo = (val) => {
+        setInfo({
+            ...info,
+            [val]: info[val] + 1
+        })
     }
 
     return (
@@ -14,6 +24,14 @@ const UseState = () => {
             <button onClick={() => setCount(prevVal => prevVal + 1)}>Increment Counter</button>&nbsp;&nbsp;
             <button onClick={() => setCount(currVal => currVal - 1)}>Decrement Counter</button>&nbsp;&nbsp;
             <button onClick={handelReset}>Reset Counter</button>
+            <hr />
+            <h1>Information</h1>
+            <p>Today I am {age} years old</p>
+            <p>I have {siblingsNum} siblings</p>
+            <div>
+                <button onClick={handelInfo.bind(null, 'age')}>Get Older!</button>&nbsp;&nbsp;
+                <button onClick={handelInfo.bind(null, 'siblingsNum')}>More Siblings!</button>
+            </div>
         </div>
     )
 }
